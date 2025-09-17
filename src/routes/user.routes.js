847145912508392b51a1cc2367aade022230f435
Rohit <rohitkumar@caplineservices.com>
2025-09-18 02:52:14 +0530

    model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const qauestionController = require('../controllers/question.controller')
 const { authMiddleware } = require('../middlewares/auth.middleware')
 
 // POST /api/users
@@ -9,9 +8,4 @@ router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
 
 router.post('/logout', authMiddleware, userController.logout)
-router.post('/question', authMiddleware, qauestionController.questionCreate);
-router.get('/getquestion', authMiddleware, qauestionController.getQuestionData)
-router.get('/getquestion/:id', authMiddleware, qauestionController.getQuestionById);
-router.delete('/deletequestion/:id', authMiddleware, qauestionController.deleteByIdQuestion)
-
 module.exports = router;
